27e920ba578d572dbca24783be2ba86b132e6c28
shuangdai <dais@kbfoo.com>
2017-03-10 19:38:53 +0800

    first commit

---
+++ src/main/java/com/tst/Mtst.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2017, kbfoo. All rights reserved.
+ */
+package com.tst;
+
+/**
+ *
+ * @author <a href="mailto:2881090362@qq.com">戴爽</a>
+ * @version 1.0 17/3/10
+ * @since 1.0
+ */
+public class Mtst {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello workd");
+    }
+}
