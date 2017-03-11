a3834951b645eea78f4b656bbdcf19675ea4e2cf
shuangdai <dais@kbfoo.com>
2017-03-11 20:56:41 +0800

    git branch tst

---
+++ src/main/java/com/tst/SayHello.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2017, kbfoo. All rights reserved.
+ */
+package com.tst;
+
+/**
+ *
+ * @author <a href="mailto:2881090362@qq.com">戴爽</a>
+ * @version 1.0 17/3/11
+ * @since 1.0
+ */
+public class SayHello {
+
+    public static void main(String[] args) {
+
+        System.out.println("============hello world");
+
+    }
+}
